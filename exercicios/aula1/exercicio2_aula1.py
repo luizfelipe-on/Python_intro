5ef449ac9639779c4c93792913bebd67fd5481e2
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-03-19 16:39:33 -0300

    Add files via upload

---
+++ exercicios/aula1/exercicio2_aula1.py
@@ -0,0 +1,13 @@
+# EXERCÍCIO 2
+
+# Sabe-se que as velocidades da luz e do som (em m/s) são, respectivamente:
+v_luz = 3*10**8
+v_som = 343
+
+# Como ambos percorrem a mesma distância d, com o som levando 3 segundos a mais (t_som = t_luz + 3), então:
+# d = 3*(10**8)*t_luz = 343*(t_luz+3)
+t_luz = 1029/299999657
+
+# Logo a distãncia d em que os fogos foram lançados é de:
+d = 3*(10**8)*t_luz
+print('a distância em que os fogos foram lançados é de', d, 'metros')
---
+++ exercicios/aula1/exercicio5_aula1.py
@@ -0,0 +1,15 @@
+# EXERCÍCIO 5
+
+# Meus valores de massa (em kg) e altura (em metros) são, respectivamente:
+M = 69
+A = 1.79
+
+# Então meu IMC é:
+IMC = M/(A**2)
+print('meu IMC é de', IMC)
+
+# Já no caso do bebê:
+M = 11
+A = 0.7
+IMC = M/(A**2)
+print('o IMC do bebê é de', IMC)
