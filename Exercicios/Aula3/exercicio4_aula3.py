18b1ffb2d0d0312763d6152b7d0e736902b1a3f5
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-03-29 11:38:14 -0300

    Update exercicio4_aula3.py

--- Exercicios/Aula3/exercicio4_aula3.py
+++ Exercicios/Aula3/exercicio4_aula3.py
@@ -3,39 +3,35 @@ def m(mi):
     m = mi*1610
     return m
 
-mi = 2
-z1 = m(mi)
-print(mi, 'milhas valem', round(z1,3), 'metros')
+print('2 milhas valem', round(m(2),3), 'metros')
+print('5 milhas valem', round(m(5),3), 'metros')
 
 # Quanto à função que converte metros em milhas:
 def mi(m):
     mi = m/float(1610) # usei o 'float' para que os resultados da divisão não fossem sempre números inteiros
     return mi
 
-m = 3220
-z2 = mi(m)
-print(m, 'metros valem', round(z2,3), 'milhas')
+print('3220 metros valem', round(mi(3220),3), 'milhas')
+print('8050 metros valem', round(mi(8050),3), 'milhas')
 
 # Como 1 h = 3600 s, a função que converte de horas para segundos é:
 def s(h):
     s = h*3600
     return s
 
-h = 3
-z3 = s(h)
-print(h, 'horas valem', round(z3,3), 'segundos')
+print('3 horas valem', round(s(3),3), 'segundos')
+print('4 horas valem', round(s(4),3), 'segundos')
 
 # Quanto à função que converte segundos em horas:
 def h(s):
     h = s/float(3600)
     return h
 
-s = 10800
-z4 = h(s)
-print(s, 'segundos valem', round(z4,3), 'horas')
+print('10800 segundos valem', round(h(10800),3), 'horas')
+print('14400 segundos valem', round(h(14400),3), 'horas')
 
 # Podemos resolver o exercício 1 da aula 1 usando as funções acima. 
-# Definamos a fórmula da velocidade em função do deslocamento e do tempo: 
+# Vamos definir a fórmula da velocidade em função do deslocamento e do tempo: 
 def v(d,t):
     v = d/float(t)
     return v
@@ -44,10 +40,13 @@ def v(d,t):
 vmed = v(mi(10*1000),h(43.5*60))
 
 # Então:
-print('a velocidade média é de', round(vmed,3), 'milhas/h')
+print('a velocidade média é de aproximadamente', round(vmed,3), 'milhas/h')
 
-# Caso a pessoa leve 30 min para percorrer 4 milhas:
+# Caso a pessoa leve 30 min para percorrer 4 milhas e deseje-se a velocidade em km/h:
+# usar a função m(mi) que converte de milhas para metros e então dividir o valor por mil para passar para km;
+# multiplicar 30 por 60 para conseguir o tempo em segundos, usando em seguida a função h(s) que converte de segundos para horas;
+# usar a função v(d,t) que dá a velocidade média em função do deslocamento e do tempo.
 vmed = v(m(4)*10**(-3),h(30*60))
 
 # Então:
-print('a velocidade média é de', round(vmed,3), 'km/h')
+print('a velocidade média é de aproximadamente', round(vmed,3), 'km/h')
