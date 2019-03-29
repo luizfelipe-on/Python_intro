2794215226f885e1b03aeb6426e4000d292ca247
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-03-29 12:03:42 -0300

    Add files via upload

---
+++ Exercicios/Aula5/exercicio1_aula5.py
@@ -0,0 +1,8 @@
+# A função para encontrar o máximo de três números é:
+def maximum(a,b,c):
+    maximum = max(a,b,c)
+    return maximum
+
+print(maximum(5,8,4))
+print(maximum(2*4*5,3*2**2,4**3))
+
