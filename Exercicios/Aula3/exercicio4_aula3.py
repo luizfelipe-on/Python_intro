6cef3e5039cfef7584a81ed76f25f80a07bf5152
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-03-29 10:12:35 -0300

    Update exercicio4_aula3.py

--- Exercicios/Aula3/exercicio4_aula3.py
+++ Exercicios/Aula3/exercicio4_aula3.py
@@ -1,9 +1,11 @@
 # Sabe-se que 1 milha = 1610 m, logo a função que converte de milhas para metros é:
 def m(mi):
     m = mi*1610
-    print(mi, 'milhas valem', round(m,3), 'metros')
+    return m
 
-m(4)
+mi = 2
+z1 = m(mi)
+print(mi, 'milhas valem', round(z1,3), 'metros')
 
 # Quanto à função que converte metros em milhas:
 def mi(m):
@@ -11,24 +13,26 @@ def mi(m):
     return mi
 
 m = 3220
-z1 = mi(m)
-print(m, 'metros valem', round(z1,3), 'milhas')
+z2 = mi(m)
+print(m, 'metros valem', round(z2,3), 'milhas')
 
 # Como 1 h = 3600 s, a função que converte de horas para segundos é:
 def s(h):
     s = h*3600
-    print(h, 'horas valem', s, 'segundos')
+    return s
 
-s(3)
+h = 3
+z3 = s(h)
+print(h, 'horas valem', round(z3,3), 'segundos')
 
 # Quanto à função que converte segundos em horas:
 def h(s):
     h = s/float(3600)
     return h
 
-s = 18000
-z2 = h(s)
-print(s, 'segundos valem', round(z2,3), 'horas')
+s = 10800
+z4 = h(s)
+print(s, 'segundos valem', round(z4,3), 'horas')
 
 # Podemos resolver o exercício 1 da aula 1 usando as funções acima. 
 # Definamos a fórmula da velocidade em função do deslocamento e do tempo: 
@@ -42,7 +46,8 @@ vmed = v(mi(10*1000),h(43.5*60))
 # Então:
 print('a velocidade média é de', round(vmed,3), 'milhas/h')
 
-# Caso a pessoa leve 30 min para percorrer 4 milhas
+# Caso a pessoa leve 30 min para percorrer 4 milhas:
+vmed = v(m(4)*10**(-3),h(30*60))
 
-
-
+# Então:
+print('a velocidade média é de', round(vmed,3), 'km/h')
