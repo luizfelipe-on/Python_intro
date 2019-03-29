01b3eacac75eab5ee218a173022e75f1e91185ec
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-03-29 16:04:57 -0300

    Create exercicio3_aula5.py

---
+++ Exercicios/Aula5/exercicio3_aula5.py
@@ -0,0 +1,9 @@
+# A função que calcula o produto de todos os elementos de uma lista é: 
+def produto(lista):
+    total = 1
+    for i in lista:
+        total = total*i
+    print total
+        
+produto([2,8,5,4])
+produto([5,3,2])
