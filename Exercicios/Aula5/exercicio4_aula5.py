6e46d9235565b5a7d8ff8061c01aa4ad3c0eb83d
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-03-30 12:16:37 -0300

    Update exercicio4_aula5.py

--- Exercicios/Aula5/exercicio4_aula5.py
+++ Exercicios/Aula5/exercicio4_aula5.py
@@ -1,9 +1,9 @@
 # Uma função que verificar se um determinado número está dentro de um certo intervalo é:
 def verificacao(x,xmin,xmax):
     if x > xmin and x < xmax:
-        print x, 'está dentro do intervalo', [xmin,xmax]
+        print(x, 'está dentro do intervalo', [xmin,xmax])
     else:
-        print x, 'não está dentro do intervalo', [xmin,xmax]
+        print(x, 'não está dentro do intervalo', [xmin,xmax])
 
 verificacao(2,1,3)
 verificacao(5,2,4)
