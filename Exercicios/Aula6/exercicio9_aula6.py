25ba51f651185695222abb298ea79f0566424b25
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-04-04 23:42:50 -0300

    Update exercicio9_aula6.py

--- Exercicios/Aula6/exercicio9_aula6.py
+++ Exercicios/Aula6/exercicio9_aula6.py
@@ -1,5 +1,9 @@
 import turtle
 
+# Alterei a função de 'square' para 'polygon' porque desejo que ela seja capaz de
+# gerar diferentes figuras geométricas, não apenas quadrados. Para tal, acrescentei
+# o parâmetro 'n', que nada mais é do que o número de lados do polígono, e agora o
+# ângulo de desvio é de (360/n) graus:
 def polygon(t,length,n):
     for i in range(n):
         t.forward(length)
@@ -10,7 +14,11 @@ def polygon(t,length,n):
 
 jn = turtle.Screen()
 joana = turtle.Turtle()
+
+# Para criar um triângulo, um quadrado e um hexágono de lado = 80 unidades, 
+# respectivamente:
 polygon(joana,80,3)
 polygon(joana,80,4)
 polygon(joana,80,6)
+
 jn.mainloop()
