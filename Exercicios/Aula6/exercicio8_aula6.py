df547563d82e751e13f24b44dce293e21225cbce
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-04-04 23:37:34 -0300

    Update exercicio8_aula6.py

--- Exercicios/Aula6/exercicio8_aula6.py
+++ Exercicios/Aula6/exercicio8_aula6.py
@@ -1,5 +1,8 @@
 import turtle
 
+# Neste caso, o lado do quadrado terá um número de unidades equivalente a 'length', 
+# que é definido dentro da função 'square'. Como desejo criar diferentes quadrados
+# separados uns dos outros, usei as funções 'penup' e 'pendown':
 def square(t,length):
     for i in range(4):
         t.forward(length)
@@ -10,7 +13,10 @@ def square(t,length):
 
 jn = turtle.Screen()
 joana = turtle.Turtle()
+
+# Para criar quadrados com lados = 40, 80 e 120, respectivamente:
 square(joana,40)
 square(joana,80)
 square(joana,120)
+
 jn.mainloop()
