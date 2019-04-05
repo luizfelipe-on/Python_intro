62c088967eb189753afcc01e4afa274800ef292f
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-04-05 00:14:33 -0300

    Update exercicio1_aula6.py

--- Exercicios/Aula6/exercicio1_aula6.py
+++ Exercicios/Aula6/exercicio1_aula6.py
@@ -3,14 +3,16 @@ import turtle
 # Para que o programa solicite ao usuário a cor de fundo desejada, 
 # pode-se usar a função 'input':
 cor_fundo = input('a cor de fundo desejada é: ')        
-     
+  
+# Abertura da janela com cor igual à cor_fundo:
 jn = turtle.Screen()      
 jn.bgcolor(cor_fundo)
 
+# Criação da tartaruga e fazendo ela se deslocar:
 joana = turtle.Turtle()    
-
 joana.forward(50)          
 joana.left(90)             
 joana.forward(30)          
 
-jn.mainloop()             
+# Para garantir que o programa só acabe quando eu fechar a janela:
+jn.mainloop()     
