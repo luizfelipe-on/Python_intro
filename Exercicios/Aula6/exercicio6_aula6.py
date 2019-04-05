d8336a94d3f2d984c49f2abcf92e402546a8219e
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-04-05 00:26:23 -0300

    Update exercicio6_aula6.py

--- Exercicios/Aula6/exercicio6_aula6.py
+++ Exercicios/Aula6/exercicio6_aula6.py
@@ -1,15 +1,18 @@
 import turtle
+
+# Abertura da janela e estabelecendo seu título:
 jn = turtle.Screen()         
 jn.title("Quatro Estrelas Verdes")
 
+# Criando a tartaruga, estabelecendo sua cor como verde e a largura da caneta como 2:
 estrela = turtle.Turtle()       
 estrela.color("green")
 estrela.pensize(2)
 
 # Para criar 4 estrelas iguais lado a lado separadas por uma pequena distância,
 # pode-se usar um laço 'for' dentro de outro: o interno para criar cada estrela,
-# o externo para fazer com que o processo interno ocorra 4 vezes, 
-# com as estrelas ficando separadas umas das outras:
+# o externo para fazer com que o processo interno ocorra 4 vezes, com as estrelas
+# ficando separadas umas das outras:
 for j in range(4):
     for i in range(5):
         estrela.forward(80)            
@@ -18,4 +21,5 @@ for j in range(4):
     estrela.forward(120)
     estrela.pendown()
 
+# Para garantir que o programa só acabe quando eu fechar a janela:
 jn.mainloop()
