bb9d6772e75c9bd4456a71a46ab99e08dc97607c
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-04-05 00:24:26 -0300

    Update exercicio5_aula6.py

--- Exercicios/Aula6/exercicio5_aula6.py
+++ Exercicios/Aula6/exercicio5_aula6.py
@@ -1,17 +1,20 @@
 import turtle
+
+# Abertura da janela e estabelecendo seu título:
 jn = turtle.Screen()         
 jn.title("Estrela Azul")
 
-# Criando a tartaruga que desenhará uma estrela azul:
+# Criando a tartaruga, estabelecendo sua cor como azul e a largura da caneta como 2:
 estrela = turtle.Turtle()       
 estrela.color("blue")
 estrela.pensize(2)
 
 # Como cada ponta de uma estrela de 5 pontas possui um ângulo interno de 36 graus,
 # o ângulo de virada para a direita da tartaruga (aqui chamada 'estrela') tem de 
-# ser 180 - 36 = 144 graus: 
+# ser (180 - 36) = 144 graus: 
 for i in range(5):
     estrela.forward(80)            
     estrela.right(144)
 
+# Para garantir que o programa só acabe quando eu fechar a janela:   
 jn.mainloop()
