4fa5d2c2b7692d3472cd2e6074cf80d1442102b6
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-04-05 00:06:45 -0300

    Update exercicio7_aula6.py

--- Exercicios/Aula6/exercicio7_aula6.py
+++ Exercicios/Aula6/exercicio7_aula6.py
@@ -1,12 +1,17 @@
 import turtle
 
-# Criando um quadrado de lado = 80 unidades usando turtle:
+# Definindo uma função que crie um quadrado de lado = 80 unidades usando tartaruga:
 def square(t):
     for i in range(4):
         t.forward(80)
         t.right(90)
 
+# Abertura da janela e criação da tartaruga: 
 jn = turtle.Screen()
 joana = turtle.Turtle()
+
+# Desenhando o quadrado:
 square(joana)
+
+# Para garantir que o programa só termine quando eu fechar a janela.
 jn.mainloop()
