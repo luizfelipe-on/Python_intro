b3012b8a3a6de607452dda57914c5187a14bc8f5
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-04-08 16:01:39 -0300

    Add files via upload

---
+++ Exercicios/Aula6/exercicio10_aula6.py
@@ -0,0 +1,31 @@
+import turtle
+import math
+
+# Definição da função 'polygon':
+def polygon(t,length,n):
+    for i in range(n):
+        t.forward(length)
+        t.left(360/n)
+    t.penup()
+    t.forward(100)
+    t.pendown()
+
+# Definição da função 'circle', que usa 'polygon', para n = 100:
+def circle(t,r):
+    n = 100
+    circumference = 2*math.pi*r
+    length = circumference/n
+    polygon(t,length,n)
+
+# Abertura da janela e criação da tartaruga: 
+jn = turtle.Screen()
+joana = turtle.Turtle()
+
+# Para obter círculos com r = 30, 50 e 80 unidades, respectivamente:
+circle(joana,30)
+circle(joana,50)
+circle(joana,80)
+
+# Para garantir que o programa só acabe quando eu fechar a janela:
+jn.mainloop()
+
