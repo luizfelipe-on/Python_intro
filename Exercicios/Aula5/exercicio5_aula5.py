7957bc1aebf8785d73c52660b9ad10566c7ddf9f
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-04-23 17:10:38 -0300

    Update exercicio5_aula5.py

--- Exercicios/Aula5/exercicio5_aula5.py
+++ Exercicios/Aula5/exercicio5_aula5.py
@@ -1,4 +1,4 @@
-# Uma forma de verificar quais números de uma lista são pares é utilizando o '%', que fornece o resto de uma divisão.
+# Uma forma de verificar quais números de uma lista são pares é utilizando o operador módulo (%), que fornece o resto de uma divisão.
 # Sendo assim, usando '% 2' para um número inteiro, se for retornado 0 então ele é par.  
 lista = [8,15,77,938,456,623]
 for n in lista:
