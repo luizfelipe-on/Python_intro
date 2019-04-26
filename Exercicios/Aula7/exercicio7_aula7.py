0f6d25da290263ae26f6d87293f35908ac9d4c41
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-04-26 17:17:55 -0300

    Add files via upload

---
+++ Exercicios/Aula7/exercicio7_aula7.py
@@ -0,0 +1,12 @@
+def recurse(n, s):
+    """ É necessário n > 0 para que a função funcione, do contrário se terá uma recursividade infinita."""
+    if n == 0:
+        print(s)
+    else:
+        recurse(n-1, n+s)
+
+# Para n > 0, a função funciona, retornando um valor inteiro.
+recurse(3, 0)
+
+# Para n <= 0, a função nunca atinge um caso-base, sendo retornado o erro de profundidade máxima atingida.
+recurse(-1, 0)
