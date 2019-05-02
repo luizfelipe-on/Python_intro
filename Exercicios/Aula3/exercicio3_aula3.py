c584fae2d3a65a8a3472a0c4d85e7c7b01da503c
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-05-02 14:28:30 -0300

    Update exercicio3_aula3.py

--- Exercicios/Aula3/exercicio3_aula3.py
+++ Exercicios/Aula3/exercicio3_aula3.py
@@ -1,7 +1,7 @@
 import math
 
 def teta_zenital(comprimento,altura):
-	# comprimento e altura da sombra em metros
+	""" comprimento e altura da sombra em metros """
 	teta_zenital = math.atan(comprimento/altura)
 	print('o ângulo zenital do Sol ao meio-dia é', round(teta_zenital,2), 'radianos ou', round(math.degrees(teta_zenital),2), 'graus')
 
