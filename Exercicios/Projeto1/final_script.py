7d902e1ec354887ff873438aa8a44a3fa1d6ce5a
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-05-13 19:36:45 -0300

    Delete final_script.py

--- Exercicios/Projeto1/final_script.py
+++
@@ -1,7 +0,0 @@
-# Importing the modules:
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Importing the datafile and saving it in the variable 'ds':
-ds = pd.read_csv('DoubleMuRun2011A.csv')
