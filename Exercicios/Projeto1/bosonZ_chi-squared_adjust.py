3f6d40ed08326544add199bcc37561b72af9f079
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-05-21 13:49:31 -0300

    Create bosonZ_chi-squared_adjust.py

---
+++ Exercicios/Projeto1/bosonZ_chi-squared_adjust.py
@@ -0,0 +1,121 @@
+# Importing the modules:
+import math
+import numpy as np
+import pandas as pd
+from scipy import special
+import matplotlib.pyplot as plt
+from scipy.stats import chisquare
+
+# Importing the datafile and saving it in the variable 'ds':
+ds = pd.read_csv('DoubleMuRun2011A.csv')
+
+# Calculating the invariant mass of all pairs of muons:
+invariant_mass = np.sqrt(2*ds.pt1*ds.pt2*(np.cosh(ds.eta1-ds.eta2)-np.cos(ds.phi1-ds.phi2)))
+
+# Defining whick peak will be adjusted:
+ajuste = int(input('Qual pico você quer ajustar? Digite 1 para J/psi, 2 para upsilon ou 3 para bóson Z: '))
+
+# If the peak of the boson Z was chosen, the following histogram can be plotted:
+if ajuste == 3:
+    print('Histograma abaixo revela o pico do bóson Z:')
+    lowerlimit = 70
+    upperlimit = 110
+    bars = 100
+    limitedmasses = invariant_mass[(invariant_mass > lowerlimit) & (invariant_mass < upperlimit)]
+    histogram = plt.hist(limitedmasses, bins=bars, range=(lowerlimit,upperlimit))
+    plt.xlabel('Invariant mass [GeV]')
+    plt.ylabel('Number of events')
+    plt.title('Boson Z peak histogram \n')
+    plt.show()
+
+# In y-axis, the number of events per bin, which can be got from the variable 'histogram'.
+# In x-axis, the centers of the bins.
+    y = histogram[0]
+    x = 0.5*(histogram[1][0:-1] + histogram[1][1:])
+
+# Defining a function that describes the Breit-Wigner distribution for the fit:
+    def breitwigner(M, FWHM, Mo, a, b, A):
+        return a*M+b+A*((2*np.sqrt(2)*Mo*FWHM*np.sqrt(Mo**2*(Mo**2+FWHM**2)))/(np.pi*np.sqrt(Mo**2+np.sqrt(Mo**2*(Mo**2+FWHM**2)))) )/((M**2-Mo**2)**2+Mo**2*FWHM**2)
+
+    print('Uma curva Breit-Wigner ajusta bem este histograma caso o usuário entre com valores adequados de FWHM, Mo, a, b, A.')
+    print('FWHM representa uma suposição inicial da largura à meia-altura;')
+    print('Mo representa uma suposição inicial da massa invariante correspondente a esta FWHM;')
+    print('a representa uma suposição inicial da inclinação usada para notar o efeito de background;')
+    print('b representa uma suposição inicial da intersecção em y usada para notar o efeito de background;')
+    print('A representa uma suposição inicial da altura da distribuição Breit-Wigner. \n')
+           
+# Initial values for the optimization in the following order:
+    print("Valores Iniciais:")
+    FWHM = float(input('Valor inicial da largura à meia-altura (FWHM): '))
+    Mo = float(input('Valor inicial da massa invariante (Mo): '))
+    a = float(input('Valor inicial da inclinação usada para notar o efeito de background (a): '))
+    b = float(input('Valor inicial da intersecção em y usada para notar o efeito de background (b): '))
+    A = float(input('Valor inicial da altura da distribuição Breit-Wigner (A): '))
+    print("")
+    initials = [FWHM, Mo, a, b, A]
+
+# Importing the module used in the optimization, running the optimization
+# and calculating the uncertainties of the optimized parameters:
+    from scipy.optimize import curve_fit
+    best, covariance = curve_fit(breitwigner, x, y, p0=initials, sigma=np.sqrt(y))
+    error = np.sqrt(np.diag(covariance))
+    
+# Taking the new parameters and optimizing them even more, until they can not be optimized anymore:
+    while abs(Mo-best[1]) != 0:
+        FWHM = best[0]
+        Mo = best[1]
+        a = best[2]
+        b = best[3]
+        A = best[4]
+        initials = [FWHM, Mo, a, b, A]
+        best, covariance = curve_fit(breitwigner, x, y, p0=initials, sigma=np.sqrt(y))
+        error = np.sqrt(np.diag(covariance))
+
+# Printing the best values and uncertainties gotten from the optimization:
+    print("Valores e Incertezas da Optimização:")
+    first = "Valor optimizado da largura à meia-altura (FWHM) = {} +- {}".format(best[0], error[0])
+    second = "Valor optimizado da massa invariante (Mo) = {} +- {}".format(best[1], error[1])
+    third = "Valor optimizado da inclinação (a) = {} +- {}".format(best[2], error[2])
+    fourth = "Valor optimizado da intersecção em y (b) = {} +- {}".format(best[3], error[3])
+    fifth = "Valor optimizado da altura da distribuição (A) = {} +- {}".format(best[4], error[4])
+    print(first)
+    print(second)
+    print(third)
+    print(fourth)
+    print(fifth)
+        
+# Plotting the histogram with the Breit-Wigner adjust:
+    plt.hist(limitedmasses, bins=bars, range=(lowerlimit,upperlimit))
+    plt.plot(x, breitwigner(x, *best), 'r-', label='Mo = {}'.format(best[1]))
+    plt.xlabel('Invariant mass [GeV]')
+    plt.ylabel('Number of events')
+    plt.title('The Breit-Wigner fit \n')
+    plt.legend()
+    plt.show()
+    
+# Determining if the invariant mass obtained in the adjust is compatible with the 
+# one that can be found in the literature:
+    M_reference = 91.187621
+    print('De acordo com a literatura, a massa do bóson é:', M_reference)
+    M_adjusted = best[1]
+    uncertainty = error[1]
+    
+    if abs(M_adjusted-M_reference) <= 2*uncertainty:
+        print('A massa de', M_adjusted, 'que obtivemos É compatível com a da literatura')
+    else:
+        print('A massa de', M_adjusted, 'que obtivemos NÃO é compatível com a da literatura')
+        
+# Applying the chi-square statistic test to verify the quality of the fit: 
+    z = [breitwigner(lowerlimit, best[0], best[1], best[2], best[3], best[4])]
+    
+    for i in range(bars - 1):
+        k = lowerlimit + i*(upperlimit - lowerlimit)/bars
+        l = breitwigner(k, best[0], best[1], best[2], best[3], best[4])
+        z.append(l)
+    
+    divergencia, valorp = chisquare(f_obs = y, f_exp = z)
+
+    print("")
+    print("É possível avaliar a qualidade do ajuste executado a partir do teste do chi-quadrado.")
+    print("Após realizar este teste, quanto menor o valor obtido, maior a qualidade do ajuste.")
+    print("Neste caso, o valor encontrado foi de aproximadamente:", round(divergencia,3))
