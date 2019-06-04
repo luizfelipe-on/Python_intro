d302c90f724fb7305e8c4d2a261768b6418d18f1
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-06-04 00:29:48 -0300

    Update exercicio3_aula10.py

--- Exercicios/Aula10/exercicio3_aula10.py
+++ Exercicios/Aula10/exercicio3_aula10.py
@@ -1,10 +1,12 @@
+# AULA 10, EXERCÍCIO 3
+
 import numpy as np
 from math import pi, sin, cos
 import matplotlib.pyplot as plt
 
 # 1) Integral de sin(x), com 0 <= x <= 2pi:
 I = cos(0)-cos(2*pi)
-print('Integral de sin(x), com 0 <= x <= 2pi, vale:', round(I,5))
+print 'Integral de sin(x), com 0 <= x <= 2pi, vale:', round(I,5)
 
 # Definindo os valores de x, y e plotando o gráfico em barras:
 x=np.linspace(0,2*pi,50)
@@ -25,24 +27,24 @@ S = 0
 for xi in x:
     S = S + sin(xi)*2*pi/50 
     
-print('Soma das 50 barras =', round(S,5))
+print 'Soma das 50 barras =', round(S,5)
 
 S1= 0
 for xi in x1:
     S1 = S1 + sin(xi)*2*pi/100
-print('Soma das 100 barras = ', round(S1,5), '\n')
+print 'Soma das 100 barras = ', round(S1,5), '\n'
 
 # 2) Integral de cos(x), com 0 <= x <= 2pi:
 I = sin(2*pi)-sin(0)
-print('Integral de cos(x), com 0 <= x <= 2pi, vale:', round(I,5))
+print 'Integral de cos(x), com 0 <= x <= 2pi, vale:', round(I,5)
 
 # Definindo os valores de x, y e plotando o gráfico em barras:
-x=np.linspace(0,2*pi,50)
+x=np.linspace(0,2*pi,500)
 y=[cos(xi) for xi in x]
-plt.bar(x,y,color="red",align="center",width=2*pi/50)
+plt.bar(x,y,color="red",align="center",width=2*pi/500)
 
 # Definindo os valores de x1, y1 e plotando a curva:
-x1=np.linspace(0,2*pi,100)
+x1=np.linspace(0,2*pi,1000)
 y1=[cos(xi) for xi in x1]
 plt.plot(x1,y1)
 plt.title('Grafico de cos(x)',fontsize=14)
@@ -53,11 +55,11 @@ plt.show()
 # Verificando se a soma de Riemann das barras é aproximadamente zero, que é o valor da integral:
 S = 0
 for xi in x:
-    S = S + cos(xi)*2*pi/50 
+    S = S + cos(xi)*2*pi/500
     
-print('Soma das 50 barras =', round(S,5))
+print 'Soma das 500 barras =', round(S,5)
 
 S1= 0
 for xi in x1:
-    S1 = S1 + cos(xi)*2*pi/100
-print('Soma das 100 barras = ', round(S1,5))
+    S1 = S1 + cos(xi)*2*pi/1000
+print 'Soma das 1000 barras = ', round(S1,5)
