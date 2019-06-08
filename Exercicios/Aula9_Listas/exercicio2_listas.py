af0ebee8663755218db886328ade71ba76bbca52
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-06-08 19:06:24 -0300

    Update exercicio2_listas.py

--- Exercicios/Aula9_Listas/exercicio2_listas.py
+++ Exercicios/Aula9_Listas/exercicio2_listas.py
@@ -1,5 +1,5 @@
+# Criando uma lista com números sequenciais e incrementos de uma unidade de 1 a 10000:
 lista = []
-for i in range(10000):
+for i in range(0,10000):
     lista.append(i+1)
-
 print(lista)
