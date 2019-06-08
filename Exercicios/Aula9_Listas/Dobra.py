6745394a09f3f7def2cabd539f00dc75441a4b6c
luizfelipe-on <48491929+luizfelipe-on@users.noreply.github.com>
2019-06-08 19:11:34 -0300

    Delete Dobra.py

--- Exercicios/Aula9_Listas/Dobra.py
+++
@@ -1,7 +0,0 @@
-class Dobrar:
-
-    def dobrar_elementos(uma_lista):
-        for (i, valor) in enumerate(uma_lista):
-            novo_elem = 2 * valor
-            uma_lista[i] = novo_elem
-        return uma_lista
